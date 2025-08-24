7dd8a0105d519bfe5fe8be4f7b10d531adfb95f3
OM Sharma <155946656+Cypher-redeye@users.noreply.github.com>
2025-08-24 21:49:47 +0530

    Fix build script pip command

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # build_files.sh
-pip install -r requirements.txt
+# This is the new, correct line
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
