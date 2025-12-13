739ac934f9ae4790c4e2ed831ad1d4057dd1d0b3
OM Sharma <155946656+Cypher-redeye@users.noreply.github.com>
2025-12-13 23:41:19 +0530

    Add temporary create-admin route

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from .views import login_view, RegisterView, logout_view, ProfileView
+from .views import login_view, RegisterView, logout_view, ProfileView, create_admin_once
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', logout_view, name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('create-admin/', create_admin_once),  # 👈 TEMP
 ]
