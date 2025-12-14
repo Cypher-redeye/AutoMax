a43b1dd10a04b8b2874e83b7e017b08b0b77f31b
OM Sharma <155946656+Cypher-redeye@users.noreply.github.com>
2025-12-14 17:37:47 +0530

    Fix Cloudinary env variable usage

--- automax/settings.py
+++ automax/settings.py
@@ -148,9 +148,9 @@ STATICFILES_STORAGE = (
 # ☁️ Media — Cloudinary (CORRECT)
 # ===============================
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": env("ddng8qahr"),
-    "API_KEY": env("636235763963398"),
-    "API_SECRET": env("aiZIb1Q2DPULJyBld90VMEZ7SK0"),
+    "CLOUD_NAME": env("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": env("CLOUDINARY_API_KEY"),
+    "API_SECRET": env("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
